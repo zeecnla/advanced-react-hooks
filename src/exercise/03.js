e3bd64bb20bb7f6a7e5e81255fe724c50245d5f9
Cesar Melchor <csrmelchor@gmail.com>
2021-03-24 12:15:11 -0700

    Starting from exercise 3 this is where i left off on my mac.

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -2,8 +2,29 @@
 // http://localhost:3000/isolated/exercise/03.js
 
 import * as React from 'react'
+import { useContext } from 'react'
 
 // 🐨 create your CountContext here with React.createContext
+const CountContext = React.createContext()
+
+function useCount(){
+  const context = React.useContext(CountContext)
+  if(!context){
+    throw new Error("useCount must be wrapped by CountProvider")
+  }
+  return context
+}
+
+function CountProvider(props){
+  const [count, setCount] = React.useState(0)
+
+  const value = [count,setCount]
+
+  return (
+    <CountContext.Provider value={value} {...props}/>
+
+  )
+}
 
 // 🐨 create a CountProvider component here that does this:
 //   🐨 get the count state and setCount updater with React.useState
@@ -13,13 +34,12 @@ import * as React from 'react'
 
 function CountDisplay() {
   // 🐨 get the count from useContext with the CountContext
-  const count = 0
+  const [count] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  // 🐨 get the setCount from useContext with the CountContext
-  const setCount = () => {}
+  const [,setCount] = useCount()
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
@@ -27,12 +47,10 @@ function Counter() {
 function App() {
   return (
     <div>
-      {/*
-        🐨 wrap these two components in the CountProvider so they can access
-        the CountContext value
-      */}
-      <CountDisplay />
-      <Counter />
+      <CountProvider>
+        <Counter />
+        <CountDisplay />
+      </CountProvider>
     </div>
   )
 }
