0fb1d0c26e6af25fb3ae5ae1206073a2144dcfcc
Cesar Melchor <csrmelchor@gmail.com>
2021-04-13 00:23:16 -0700

    completed advanced react hooks

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -3,10 +3,14 @@
 
 import * as React from 'react'
 
+const formatCountDebugValue = ({query, state}) => `\`${query}\` => ${state}`
+
 function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState)
   // 🐨 call React.useDebugValue here.
   // 💰 here's the formatted label I use: `\`${query}\` => ${state}`
+
+  React.useDebugValue({query, state}, formatCountDebugValue)
 
   React.useEffect(() => {
     let mounted = true
